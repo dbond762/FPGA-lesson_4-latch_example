51078e27851f9626c18cd0be88247ba3ca647a23
Dmytro Bondarenko <dbond762@gmail.com>
2026-09-05 14:45:16 +0300

    write latch_example module

---
+++ HW_4_2.srcs/sources_1/new/latch_example.v
@@ -0,0 +1,40 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05.09.2026 14:13:11
+// Design Name: 
+// Module Name: latch_example
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module latch_example(
+    input  wire [1:0] sel,
+    input  wire [3:0] data,
+    output reg        out
+    );
+    
+    always_comb begin
+        case (sel)
+            2'b00: out = data[0];
+            2'b01: out = data[1];
+            2'b10: out = data[2];
+            //2'b11: out = data[3];
+            
+            //default: out = 0;
+        endcase
+    end
+    
+endmodule
