cd922ce4110b3e9445dde5a803a3094e7a2852c2
Dmytro Bondarenko <dbond762@gmail.com>
2026-09-05 14:47:32 +0300

    add comment for demonstration latch example

--- HW_4_2.srcs/sources_1/new/latch_example.v
+++ HW_4_2.srcs/sources_1/new/latch_example.v
@@ -31,8 +31,8 @@ module latch_example(
             2'b00: out = data[0];
             2'b01: out = data[1];
             2'b10: out = data[2];
+            // Commented for demonstrate latch example
             //2'b11: out = data[3];
-            
             //default: out = 0;
         endcase
     end
